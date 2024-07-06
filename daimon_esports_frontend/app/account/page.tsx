7ab1d43e764c9f68d42a44ca7e56a921a0116965
mbentity <dariobizzarri02@gmail.com>
2024-07-06 14:59:57 +0200

    added deleteaccount

--- daimon_esports_frontend/app/account/page.tsx
+++ daimon_esports_frontend/app/account/page.tsx
@@ -3,6 +3,7 @@
 import { HomeLink } from "@/app/commons";
 import { useGlobalContext } from "@/app/Context/store";
 import { useEffect } from "react";
+import axios from "axios";
 
 export default function Account () {
     const { setUser, authenticated, setAuthenticated } = useGlobalContext();
@@ -27,10 +28,22 @@ export default function Account () {
         location.href = "/account/login";
     };
 
+    const handleDeleteAccount = () => {
+        axios({
+            method: "delete",
+            url: process.env.NEXT_PUBLIC_BACKEND_ENDPOINT+"/user/",
+            withCredentials: true
+        })
+            .then(() => {
+                location.href = "/";
+            });
+    }
+
     return (
         <div>
             <h1>Account</h1>
             <button onClick={handleLogout}>Logout</button>
+            <button onClick={handleDeleteAccount}>Delete Account</button>
             <HomeLink/>
         </div>
     );
